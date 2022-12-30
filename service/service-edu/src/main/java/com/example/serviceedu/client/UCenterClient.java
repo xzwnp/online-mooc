4d1c48014878017d3f50703ae0c373c2ca5c0c1e
xzwnp <2633247964@qq.com>
2022-12-30 15:05:29 +0800

    在线课程学习平台

---
+++ service/service-edu/src/main/java/com/example/serviceedu/client/UCenterClient.java
@@ -0,0 +1,23 @@
+package com.example.serviceedu.client;
+
+import com.example.commonutils.vo.UserInfoOrderVo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * com.example.serviceorder.client
+ *
+ * @author xzwnp
+ * 2022/4/2
+ * 11:50
+ * Steps：
+ */
+@Component("ucenter")
+@FeignClient("service-ucenter")
+public interface UCenterClient {
+    //根据课程id查询课程信息
+    @GetMapping("/educenter/member/userOrderInfo/{id}")
+    UserInfoOrderVo getInfo(@PathVariable("id") String id);
+}
